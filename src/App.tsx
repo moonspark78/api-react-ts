d87371ee1f8dec0a5560c59020cc876de4008b0f
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2023-04-03 16:04:18 +0200

    modify the then

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,21 @@ import './App.css';
 function App() {
   const [todos, setTodos] = useState([])
 
+  interface Todo {
+    title : string;
+    id: number;
+    completed: boolean;
+  }
+
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/todos/")
       .then(response => response.json())
-      .then(json => console.log(json.slice(0,10)))
+      .then(json => setTodos(json.slice(0,10)))
+      .catch(err => console.log(err)) 
   },[])
   return (
     <div className="App">
-      Hello Word
+      {todos.length > 0 ? todos.map((todo: Todo) => console.log(todo.title)) : ("Loading...")}
     </div>
   );
 }
