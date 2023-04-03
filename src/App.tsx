d42711d8671a5edb7ef6c9cbfe6888638fbbeb0b
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2023-04-03 15:50:13 +0200

    modify the then

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import React, { useEffect, useState } from 'react';
-import logo from './logo.svg';
+import  { useEffect, useState } from 'react';
+
 import './App.css';
 
 function App() {
   const [todos, setTodos] = useState([])
 
   useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/todos")
-    .then(response => response.json)
-    .then(res => console.log(res))
+    fetch("https://jsonplaceholder.typicode.com/todos/")
+      .then(response => response.json())
+      .then(json => console.log(json.slice(0,10)))
   },[])
   return (
     <div className="App">
