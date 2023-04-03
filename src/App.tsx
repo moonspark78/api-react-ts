d1b8f023a52449cab09838181fe02acec194737a
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2023-04-03 15:26:51 +0200

    setup useState and usEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [todos, setTodos] = useState([])
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/todos")
+  },[])
   return (
     <div className="App">
       Hello Word
